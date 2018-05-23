de634a63c5ad85388e71ba49ed7731af53fc7f75
Joana Reis <joanarfreis@gmail.com>
2018-05-23 16:21:18 +0100

    Yelp finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit
+  before_action :set_restaurant, only: [:show, :edit]
+
   def index
   @restaurants = Restaurant.all
   end
@@ -7,9 +8,27 @@ class RestaurantsController < ApplicationController
   def show
   end
 
-  def new
+ def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def create
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
